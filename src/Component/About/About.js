479a9661d4979526a3ac689e9dceb4e674a308d0
sabbir0029 <mdsabbirhossain0029@gmail.com>
2021-10-04 20:58:28 +0600

    commit

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col,} from 'react-bootstrap';
+import img from '../../images/20943839.jpg'
 
 const About = () => {
   return (
     <div className='container mb-5'>
       <h1 className='bg-danger p-5 text-center text-light rounded-3 mb-5 mt-5'>ABOUT US</h1>
-      <div>
-      <Row xs={1} md={2} className="g-4">
-          {Array.from({ length: 4 }).map((_, idx) => (
-            <Col>
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+      <div className='m-5'>
+      <Row>
+        <Col className='p-5 fs-3 fw-bold'>
+        <p>Education disciplines and develops both academic performances as well as curricula activities. A person learns to read, weite ,speak,practice activities, and learn various skills through Education. Education makes a person independent and self-sufficient.Learning is a part of Education and can be learned for anyone around us.</p>
+        </Col>
+        <Col>
+        <img src={img} className='w-75 p-4' alt="" />
+        </Col>
       </Row>
       </div>
     </div>
--- src/Component/NotFount/NotFount.js
+++ src/Component/NotFount/NotFount.js
@@ -3,7 +3,8 @@ import img from '../../images/requested-page-found-404-error_81534-2158.jpg'
 
 const NotFount = () => {
   return (
-    <div className='text-center'>
+    <div className='text-center mt-5'>
+      <h1>NOT FOUNT</h1>
       <img src={img} alt="" />
     </div>
   );
